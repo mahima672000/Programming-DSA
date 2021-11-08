3819e054f72eb79b7a38a293f269f85a69c6d6d8
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-08 22:30:45 +0530

    Create Print_inJava.java

---
+++ Print_inJava.java
@@ -0,0 +1,39 @@
+/* method 1*/
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Expectation {
+    System.out.print("Mahima\n");
+    System.out.print("Sahu");
+      
+      /* method 2*/
+      
+      
+ import java.util.*;
+    
+ public class Main {
+   public static void main(String[] args) throws Expectation {
+     System.out.println("Mahima");
+     System.out.println("Sahu");
+     
+     
+        /* method 3*/
+     
+import java.util.*;
+ 
+public class Main{
+  public static void main(String[] args) throws Expectation {
+    System.out.print("Mahima");
+    System.out.print("Sahu");
+    
+    
+    
+    /* OUTPUT
+    method 1 -  Mahima
+                Sahu
+                
+        method 2 - Mahima
+                   Sahu
+                   
+         method 3 - MahimaSahu  */
+                
