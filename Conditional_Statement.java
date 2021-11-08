ead64edf7a23d766e5d1ebee1bad052ce4ddf336
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-09 01:02:01 +0530

    Create Conditional_Statement.java

---
+++ Conditional_Statement.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    int x = 10;
+
+    if (x % 2 == 0) {
+      System.out.println("The number is even");
+    } else {
+      System.out.println("The number is odd");
+    }
+    System.out.println("always executed because it is not inside any conditional statement hehe");
+  }
+
+}
+/* according to this program that we have written above, it will display that 0 is even but 0 is neither even nor odd */
+
+import java.util.*;
+
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    int x = 11;
+
+    if (x % 2 == 0) {
+
+      if (x == 0) {
+        System.out.println("Neither Even Nor Odd");  /* 0 comes under x%2==0 */
+      } else {
+        System.out.println("The number is even");
+      }
+
+    } else {
+      System.out.println("The number is odd");
+    }
+    System.out.println("always executed because it is not inside any conditional statement hehe");
+  }
+
+}
