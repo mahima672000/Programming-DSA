3e2eca8cd419f632b9648a7497b3cba45b29c5e8
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-08 22:37:59 +0530

    Create Print_Z.java

---
+++ Print_Z.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("*****");
+    System.out.println("   *"); /* 3 space 1 star*/
+    System.out.println("  *"); /* 2 spaces 1 star*/
+    System.out.println(" *"); /* 1 space 1 star*/
+    System.out.println("*****");
+  }
+}
