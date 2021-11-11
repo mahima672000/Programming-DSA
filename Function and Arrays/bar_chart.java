58436d965234c6fb92bf1d3d8a0e26a9059db0f2
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-11 19:09:53 +0530

    Update bar_chart.java

--- Function and Arrays/bar_chart.java
+++ Function and Arrays/bar_chart.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // not taking input from user
     int n = 5; 
     int[] arr = {3, 1, 0, 7, 5}; // inputing a known array !
 
