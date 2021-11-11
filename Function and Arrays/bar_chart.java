fbe6e259d389f38b582dbdc51757f1fff7165c81
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-11 18:55:01 +0530

    Create bar_chart.java

---
+++ Function and Arrays/bar_chart.java
@@ -0,0 +1,35 @@
+/* given an array of n elements. Each element depicts the height of a vertical bar. print output */
+
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = 5; 
+    int[] arr = {3, 1, 0, 7, 5}; // inputing a known array !
+
+
+    int max = Integer.MIN_VALUE; // intialising the maximum 
+    for (int i = 0; i < n; i++) {
+
+      max = Math.max(max, arr[i]); // maximum is maximum  of arr[i]
+    }
+
+
+    for (int i = 0; i < max; i++) { // outerloop for all floors
+      for (int val : arr) { // val belonging to arr ie arr elements not using arr[i] coz outer for loop is using i for iteration
+        if (val >= max - i) {
+          System.out.print("*	");
+        } else {
+          System.out.print("	");
+        }
+      }
+
+      System.out.println();
+    }
+  }
+
+}
