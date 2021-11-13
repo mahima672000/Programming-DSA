0862ac88c11ce7a8672535750cdb5bf4b201379d
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-14 00:52:15 +0530

    Update rotate_an_array.java

--- Function and Arrays/rotate_an_array.java
+++ Function and Arrays/rotate_an_array.java
@@ -10,7 +10,56 @@ Reverse the entire Array: Now, reverse the entire array i.e. from 0 to N-1. The 
 
 /* K<N
    case-1 K>0
-   rotate it with K only
+   rotate it with K%N only
    
    case-2 K<0
-   rotate it with
+   rotate it with N+K%N */
+
+
+import java.io.*;
+
+import java.util.*;
+public class Main {//main class
+  public static void display(int[] a) {//display fn
+    StringBuilder sb = new StringBuilder();
+
+    for (int val : a) {
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+public static void reverse(int[] a, int li, int ri) {//reverse fn
+    while (li < ri) {
+      int temp = a[li];
+      a[li] = a[ri];
+      a[ri] = temp;
+
+      li++;
+      ri--;
+    }
+  }
+  public static void rotate(int[] a, int k) {//rotate fn
+    k = k % a.length;
+    if (k < 0) {
+      k += a.length;
+    }
+
+    reverse(a, 0, a.length - k - 1);
+    reverse(a, a.length - k, a.length - 1);
+    reverse(a, 0, a.length - 1);
+  }
+  public static void main(String[] args) throws Exception {//main fn
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    rotate(a, k);
+    display(a);
+  }
+
+}
