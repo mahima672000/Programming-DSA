af50b73d02f89bff050b9700650c73974e51b7e1
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-14 01:09:27 +0530

    Create inverse_of_an_array.java

---
+++ Function and Arrays/inverse_of_an_array.java
@@ -0,0 +1,10 @@
+/* given a number n, representing the size of array a,
+followed by n numbers, representing elements of array a.
+
+You are required to calculate the inverse of array a. 
+Since input and output are already managed, your only work is 
+to code a function that will return the user a new array with inverted values. */
+
+/* The basic concept of the problem is to change the positions of the
+values as their indices and indices as values using a for loop and a new array of the same length as that of the input array's length . */
+
