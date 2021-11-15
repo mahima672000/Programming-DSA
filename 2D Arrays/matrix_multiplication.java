f8ed2a1a81844efd2ca18ae8546562646a50b04d
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-15 16:54:42 +0530

    Update matrix_multiplication.java

--- 2D Arrays/matrix_multiplication.java
+++ 2D Arrays/matrix_multiplication.java
@@ -2,3 +2,5 @@
 Here, n1=no. of rows and m1=no. of columns of the First Matrix. And, n2=no. of rows and m2=no.of columns of the Second Matrix.
 
 Only if m1=n2, then a product matrix can be formed of size n1*m2. */
+
+
