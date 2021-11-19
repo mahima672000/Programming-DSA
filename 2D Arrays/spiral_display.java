21e18e6d1048a32091a142ec7f9fd4ee1f7c6a3d
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-20 00:30:51 +0530

    Create spiral_display.java

---
+++ 2D Arrays/spiral_display.java
@@ -0,0 +1,8 @@
+/* iven a number n, representing the number of rows, then a number m, representing the number of columns.
+After that you will be given n*m numbers, representing elements of 2d arrays.
+
+We need to traverse this input 2D array, in a spiral pattern 
+
+To traverse the 2D array, we use the trick that instead of traversing the 2D array actually in a spiral pattern,
+we may think of traversing it box by box making sure that value at corner doesn't repeat.*/
+
