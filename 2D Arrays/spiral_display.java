037e88c9f24d5f29a4946d2ccc360142257d3326
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-11-27 23:41:05 +0530

    Update spiral_display.java

--- 2D Arrays/spiral_display.java
+++ 2D Arrays/spiral_display.java
@@ -6,3 +6,31 @@ We need to traverse this input 2D array, in a spiral pattern
 To traverse the 2D array, we use the trick that instead of traversing the 2D array actually in a spiral pattern,
 we may think of traversing it box by box making sure that value at corner doesn't repeat.*/
 
+import java.io.*;
+
+import java.util.*;
+public class Main // min class
+{
+  public static void main (String[]args) throws Exception //main fn
+  {
+    Scanner scn = new Scanner (System.in);
+    int n = scn.nextInt ();
+    int m = scn.nextInt ();
+    int arr[][] = new int[n][m]; //initialising & declaring the array
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = 0; j < arr[i].length; j++)
+      {
+        arr[i][j] = scn.nextInt ();
+      }
+    }
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = 0; j < arr[i].length; j++)
+      {
+        System.out.print (arr[i][j] + " ");
+      }
+      System.out.println ();
+    }
+  }
+}
