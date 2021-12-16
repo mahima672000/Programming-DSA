187a8d632bc905a4087ccf2816fb731a7f67b54a
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-16 21:06:34 +0530

    Create print_decreasing.java

---
+++ introduction-to-recursion/print_decreasing.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = scn.nextInt();
+    printDecreasing(n);
+  }
+
+  public static void printDecreasing(int n) {
+    //Base case
+    if (n == 0)
+      return;
+
+    //Self Work
+    System.out.println(n);
+
+    //Recursive call
+    printDecreasing(n - 1);
+  }
+}
+
+/*don't count wipe out ones in call
+
+Time Complexity: O(n)
+As n calls are made and work is done corresponding to these n calls therefore the time complexity becomes O(n).
+
+Space Complexity: O(1)
+Since no extra space is used, therefore space complexity is constant, 
+however you should know that if the recursion call stack is taken into account, then space complexity will be O(n) as there are n recursive calls. */
+/*DOUBT*/
