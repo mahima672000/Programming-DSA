535e430a57a1e53c0e31021c6444bd2d3d2461b4
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-17 21:33:27 +0530

    Update print_zigzag.java

--- introduction-to-recursion/print_zigzag.java
+++ introduction-to-recursion/print_zigzag.java
@@ -1,2 +1,17 @@
 /* we are given an integer n and are required to print a particular type of sequence of numbers.*/
 
+import java.io.*;
+import java.util.*;
+
+print class Main{// main class
+  public static void main(String[] args)throws Exception{// main function
+  }
+  
+  
+  public static void zigzag(int n){//zigzag function
+    if (n==0){
+      return;
+    }
+    
+    System.out.print(n + "");// not println because we have to print in the same line
+    zigzag(n-1);
