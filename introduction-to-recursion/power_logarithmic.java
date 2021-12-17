2d74aea51a60fa417cdeb9c5fe408a0685c8d972
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-17 20:25:18 +0530

    Create power_logarithmic.java

---
+++ introduction-to-recursion/power_logarithmic.java
@@ -0,0 +1,3 @@
+/* we are using logarithmic so as to decrease the time complexity of linear approach */
+
+/* Hint: Try to think about the divide and conquer strategy. Try to divide the problem into fewer subproblems.*/
