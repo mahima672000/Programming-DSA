850bd7bd709358d253f45a3e81cc74e898e36e38
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-17 14:42:21 +0530

    Update factorial.java

--- introduction-to-recursion/factorial.java
+++ introduction-to-recursion/factorial.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Main {
    public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
+    Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     printFactorial(n);
   }
