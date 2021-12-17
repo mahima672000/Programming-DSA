69b02f40a575d76d902663fc5387d8505afb4432
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-17 14:40:37 +0530

    Create factorial.java

---
+++ introduction-to-recursion/factorial.java
@@ -0,0 +1,21 @@
+/* factorial of both 0 and 1 is 1 
+Factorials of negative integers do not exist */
+
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+   public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = scn.nextInt();
+    printFactorial(n);
+  }
+  
+  public static int printFactorial(int n){ // fn type int so that return type int so that we can multiply
+    if(n==1){
+      return 1; // value is returned isliye '}' wipe out case me not needed
+    }
+    return n*(printFactorial(n-1));
+  }
+}
