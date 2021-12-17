4fbed6a590ff974f8a51b36833ba782d42495c84
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-17 20:17:49 +0530

    Update power_linear.java

--- introduction-to-recursion/power_linear.java
+++ introduction-to-recursion/power_linear.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // taking multiple inputs
         int x =Integer.parseInt(br.readLine());
-        int n =Integer.parseInt(br.readline());
+        int n =Integer.parseInt(br.readLine());
         int p =power(x,n);
         System.out.println(p);
     }
@@ -20,4 +20,23 @@ public class Main {
     return x*power(x,n-1);
     }   
 } 
-/* */
+/*
+Time Complexity :
+O(n)
+
+Since you are recursively calling for a subproblem with n-1 from n,
+hence n + 1 recursive calls will be made (+ 1 when it hits the base case n = 0). 
+You can also count the number of recursive calls in the call stack. 
+So the time complexity turns out to be O(n) (which is independent of the value of x).
+
+SPACE COMPLEXITY :
+O(1)
+
+There is no data structure used, thus no auxiliary space is used. Hence, space complexity is O(1).
+Note: There are n recursive calls that do take stack space, 
+but we do not account for this space as we are only concerned with space used by data structures defined by us.
+However, if it is mentioned to give the space complexity 
+which takes into account the space taken by recursion call stack also, 
+then, since there are n recursive calls, hence O(n) space will be required.
+
+*/
