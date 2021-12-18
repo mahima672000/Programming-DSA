fec47925a66162134cfe037df10dd78ffd755e69
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-18 13:55:30 +0530

    Update print_zigzag.java

--- introduction-to-recursion/print_zigzag.java
+++ introduction-to-recursion/print_zigzag.java
@@ -10,8 +10,18 @@ print class Main{// main class
   
   public static void zigzag(int n){//zigzag function
     if (n==0){
-      return;
+      return; // wipe out
     }
     
     System.out.print(n + "");// not println because we have to print in the same line
     zigzag(n-1);
+    System.out.print(n+"");
+    zigzag(n-1);
+    System.out.print(n+"");
+  }
+  
+  // n + zigzag(n-1) + n + zigzag(n-1) + n ...considering all string ...this is Output Format
+  //n +"" this converts or we can say prints n as a string.
+}
+  
+
