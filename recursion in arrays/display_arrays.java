5770d1704cd297a0d0db6dc310081b730e622025
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-19 14:09:30 +0530

    Update display_arrays.java

--- recursion in arrays/display_arrays.java
+++ recursion in arrays/display_arrays.java
@@ -1,4 +1,17 @@
-/* display arrays not in iterative way but in recursive way */
+/* display arrays not in iterative way but in recursive way 
+In general, BufferedReader comes in handy if we want to read text from any kind of input source whether that be files, sockets, or something else.
+we could use the Scanner class to achieve the same functionality as with BufferedReader.
+
+there are significant differences between these two classes which can make them either more or less convenient for us, depending on our use case:
+
+BufferedReader is synchronized (thread-safe) while Scanner is not
+Scanner can parse primitive types and strings using regular expressions
+BufferedReader allows for changing the size of the buffer while Scanner has a fixed buffer size
+BufferedReader has a larger default buffer size
+Scanner hides IOException, while BufferedReader forces us to handle it
+BufferedReader is usually faster than Scanner because it only reads the data without parsing it
+With these in mind, if we are parsing individual tokens in a file, 
+then Scanner will feel a bit more natural than BufferedReader. But, just reading a line at a time is where BufferedReader shines.*/
 
 import java.io.*;
 import java.util.*;
