a1730c202a05fd4bba53124a0738205877da8ba0
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-19 15:56:00 +0530

    Create introduction_to_functions.java

---
+++ Function and Arrays/functions/introduction_to_functions.java
@@ -0,0 +1,4 @@
+/* To avoid this redundancy of code, we use functions or methods.
+
+A function is a block of code that executes when called 
+and that does a particular task whenever you call it. A function is always written inside a class.
