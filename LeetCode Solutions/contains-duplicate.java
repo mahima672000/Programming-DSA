aa19f54343593cac5f0de3fe5d307162c8bd4130
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-21 05:18:03 +0530

    Create contains-duplicate.java

---
+++ LeetCode Solutions/contains-duplicate.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i = 0; j = nums.length-1; (i<j); i++; j--){
+            int counter = 0;
+            if(nums[i]==nums[j]){
+                counter++;
+            }
+            if(counter>0){
+                return true;
+            }
+            else{
+                return false;
+            }
+                
+            
+        }
+        
+        
+    }
+}
