193d878bc4dc83b4b2dee0de21900b3b5e717f60
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-20 20:50:24 +0530

    Create sum_of_two_arrays.java

---
+++ Function and Arrays/arrays/sum_of_two_arrays.java
@@ -0,0 +1,6 @@
+/* Pseudo Code:
+Find out "max", the maximum of all the elements.
+The outer loop runs for each "floor" from max to 1.
+The inner loop runs for each element of the array or "height" from index 0 to arr.length-1.
+If arr[i]/ height> floor then print star, else print blank. */
+
