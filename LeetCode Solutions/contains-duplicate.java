ad52d17139537b0bd9a6587eebe5fb4cdf8be59c
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-21 05:23:35 +0530

    Update contains-duplicate.java

--- LeetCode Solutions/contains-duplicate.java
+++ LeetCode Solutions/contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        for(int i = 0; j = nums.length-1; (i<j); i++; j--){
+        for(int i = 0; j = nums.length-1; i<j; i++; j--){
             int counter = 0;
             if(nums[i]==nums[j]){
                 counter++;
