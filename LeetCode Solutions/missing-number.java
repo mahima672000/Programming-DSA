6ae1236950f28b0e08d966920e382a934c57c021
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-21 05:35:32 +0530

    Create missing-number.java

---
+++ LeetCode Solutions/missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        for(int i = 0 ; i<=n ;i++ ){
+            if(int val:nums != i){
+                return i;
+            }
+        }
+    }
+}
