d6028b01abe39ebe2dae3633b874377b77113e1a
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-22 00:25:24 +0530

    Create string_compression.java

---
+++ string,-string-builder-and-arraylist/string_compression.java
@@ -0,0 +1,4 @@
+/* The string should be compressed such that the
+consecutive duplicates of characters are replaced with a single character. 
+The string should be compressed such that the consecutive duplicates 
+of characters are replaced with a single character and followed by the number of consecutive duplicates.
