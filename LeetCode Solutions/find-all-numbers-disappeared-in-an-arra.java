5cb622d449895eb3ede80ea7363de71159f5a036
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-21 05:52:34 +0530

    Create find-all-numbers-disappeared-in-an-arra.java

---
+++ LeetCode Solutions/find-all-numbers-disappeared-in-an-arra.java
@@ -0,0 +1,17 @@
+/*for something like this elements are repeated in nums */
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for(int i = 0 ; i<=n ; i ++){
+            int c=0;
+            if(i != val:nums){
+                c++;
+              int arr[] = new int[c]
+               for(int j=0;j<c;j++){
+                   arr[j]=i
+                    return arr;
+               }   
+            }
+        }
+        
+    }
+}
