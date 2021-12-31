070e8b8e85b4af977932038867fe676045caf6c3
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2021-12-31 13:46:01 +0530

    Create climb_stairs.java

---
+++ dynamic-programming-and-greedy/climb_stairs.java
@@ -0,0 +1,4 @@
+/*You are given a number n, representing the number of stairs in a staircase.
+You are on the 0th step and are required to climb to the top.
+In one move, you are allowed to climb 1, 2, or 3 stairs.
+You are required to print the number of different paths via which you can climb to the top.*/
