539f5f341166b1085dd46ddf2af13ccde5c41fca
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-01 15:33:38 +0530

    Update string_compression.java

--- string,-string-builder-and-arraylist/string_compression.java
+++ string,-string-builder-and-arraylist/string_compression.java
@@ -31,7 +31,36 @@ public class Main{
     return ans;
   }
       
-
+/* WITH FREQUENCY COUNT */
+  import java.io.*;
+  import java.util.*;
+  
+  public class Main{
+    public static void main(String[]args){
+      Scanner scn = new Scanner(System.in);
+      String s = scn.next();
+      System.out.println(frequencyCompression);
+    }
+    public static String frequencyCompression(String s){// String is the return type here
+      String ans = "";
+      for(int i=0; i<s.length();i++){//i<s.length() coz only then it will be 0 to s.length-1
+        while(i<s.length()-1 && s.charAt(i) == s.charAt(i+1)){//coz u shud check from this statement `s.charAt(i) == s.charAt(i+1)`  i+1 should be at max s.length()-1 so i <s.length()-1
+          count++;
+          i++;
+        }
+        ans +=s.charAt(i);
+        if(count>1){
+          ans+=count;
+        }
+      }
+      return ans;
+    }
+    
+    /* Time Complexity :
+In both ways, we are traversing the input string once, hence the time complexity will be O(n)
+SPACE COMPLEXITY :
+If we take the space of the output string, then the solution is taking O(n) space. 
+Although, if we ignore the space taken by the output, (which we generally do), the solution is said to take O(1) auxiliary space. */
 
 
 
