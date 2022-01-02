9c4afd5ab5a74f5db7b1d1a5fd8888de1e89450c
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-02 09:37:47 +0530

    Update String_builder_usage_and_performance.java

--- string,-string-builder-and-arraylist/String_builder_usage_and_performance.java
+++ string,-string-builder-and-arraylist/String_builder_usage_and_performance.java
@@ -32,3 +32,36 @@ Replaces the substring contained between start and end index with the specified 
 
 sb.toString()
 Returns a string representing the contents of sb (StringBuilder object). */
+
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("Hello");
+    System.out.println("String : " + sb);
+    // 1.
+    System.out.println("Length of string : " + sb.length());
+    // 2.
+    System.out.println("First character : " + sb.charAt(0));
+    // 3.
+    sb.setCharAt(0, 'h');
+    System.out.println("String : " + sb);
+    // 4.
+    sb.append(" reader");
+    System.out.println("String : " + sb);
+    //5.
+    sb.deleteCharAt(5);
+    System.out.println("String : " + sb);
+    // 6.
+    sb.delete(0, 5);
+    System.out.println("String : " + sb);
+    // 7.
+    sb.replace(4, 6, "ing");
+    System.out.println("String : " + sb);
+    // 8.
+    String str = sb.toString();
+    System.out.println("String : " + str);
+  }
+}
+
+
