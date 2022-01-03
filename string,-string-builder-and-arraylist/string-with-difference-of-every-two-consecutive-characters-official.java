2616279b3eea462bb2408ae502b4dc43afc5cf29
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-03 12:35:46 +0530

    Create string-with-difference-of-every-two-consecutive-characters-official.java

---
+++ string,-string-builder-and-arraylist/string-with-difference-of-every-two-consecutive-characters-official.java
@@ -0,0 +1,32 @@
+/*1. You are given a string that contains only lowercase and uppercase alphabets. 
+2. You have to form a string that contains the difference of ASCII values of every two consecutive characters between those characters.
+   For "abecd", the answer should be "a1b3e-2c1d", as 
+   'b'-'a' = 1
+   'e'-'b' = 3
+   'c'-'e' = -2
+   'd'-'c' = 1 */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String solution(String str){
+	    StringBuilder sb = new StringBuilder(str);
+	    String ans = "";
+	    for(int i=0;i<sb.length;i++){
+	        char ch1 = sb.charAt(i);
+	        char ch2 = sb.charAt(i+1);
+	        ans += ch1;
+	        ans += (char)('ch2'-'ch1');
+	        
+	    }
+	    return ans;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}
