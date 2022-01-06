931c88af3302a6edf1937e52897c7fb1c846ee4a
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-06 15:34:29 +0530

    Create balanced_brackets.java

---
+++ stacks-and-queues/balanced_brackets.java
@@ -0,0 +1,4 @@
+/* An expression will have unbalanced brackets if the order of the brackets does not match, 
+or if the opening brackets are more as compared to the closing brackets 
+or the closing brackets are more as compared to the opening brackets. */
+
