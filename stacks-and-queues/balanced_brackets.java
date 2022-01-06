2bb5359dd871100e91240429411e54d88089c15c
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-06 16:50:56 +0530

    Create balanced_brackets.java

---
+++ stacks-and-queues/balanced_brackets.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void main(String[]args) throws Exception{
+    Scanner scn = new Scanner(System.in);
+    String str =scn.nextLine();
+    Stack<Character> st = new Stack<>();
+    
+    for(int i=0;i<str.length();i++)
+    {
+      
