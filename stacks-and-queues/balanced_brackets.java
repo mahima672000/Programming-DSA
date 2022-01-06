d97f8648089baed80753650a621c5782d30aae41
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-06 19:40:33 +0530

    Update balanced_brackets.java

--- stacks-and-queues/balanced_brackets.java
+++ stacks-and-queues/balanced_brackets.java
@@ -1,20 +1,80 @@
 import java.io.*;
+
 import java.util.*;
 
-public class Main{
-  public static void main(String[]args) throws Exception{
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
     Scanner scn = new Scanner(System.in);
-    String str =scn.nextLine();
-    Stack<Character> st = new Stack<>();
-    
-    for(int i=0;i<str.length();i++)
+    String str = scn.nextLine();
+    Stack< Character> st = new Stack<>();
+
+    for (int i = 0; i < str.length(); i++)
     {
       char ch = str.charAt(i);
-      if(ch==')'||ch==']'||ch=='}')
+      if (ch == '(' || ch == '[' || ch == '{')
       {
         st.push(ch);
       }
-      else if(ch==')')
+      else if (ch == ')')
       {
-        
-      
+        boolean val = handleClosing(st, '(');
+        if (val == false)
+        {
+          System.out.println(val);
+          return;
+        }
+      }
+      else if (ch == ']')
+      {
+        boolean val = handleClosing(st, '[');
+        if (val == false)
+        {
+          System.out.println(val);
+          return;
+        }
+      }
+      else if (ch == '}')
+      {
+        boolean val = handleClosing(st, '{');
+        if (val == false)
+        {
+          System.out.println(val);
+          return;
+        }
+      }
+      else
+      {
+
+      }
+    }
+    if (st.size() == 0)
+    {
+      System.out.println(true);
+    }
+    else
+    {
+      System.out.println(false);
+    }
+
+
+  }
+  public static boolean handleClosing(Stack < Character> st, char corresopch)
+  {
+    if (st.size() == 0)
+    {
+      return false;
+    }
+    else if (st.peek() != corresopch)
+    {
+      return false;
+    }
+    else
+    {
+      st.pop();
+      return true;
+    }
+  }
+
+}
