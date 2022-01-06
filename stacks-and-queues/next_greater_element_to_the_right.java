3b8540723b56f799e2733ba91a1d8dee94992fc3
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-06 15:05:19 +0530

    Update next_greater_element_to_the_right.java

--- stacks-and-queues/next_greater_element_to_the_right.java
+++ stacks-and-queues/next_greater_element_to_the_right.java
@@ -44,10 +44,21 @@ public class Main{
     nge[arr.length-1] = -1;
     st.push(arr[arr.length-1]);
     
-    for(int i = arr.length-2 ; i>=0 ;i--){
-      while(st.size()>0&&
-   return null;
- }
+    for (int i = arr.length - 2; i >= 0; i--) {
 
+    while (st.size() > 0 && arr[i] >= st.peek()) {
+      st.pop();
+    }
+
+    if (st.size() == 0) {
+      nge[i] = -1;
+    } else {
+      nge[i] = st.peek();
+    }
+
+    st.push(arr[i]);
+  }
+
+  return nge;
 }
     
