3d70969222a6fc1afb0e4e588d79a28f8de502ce
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-10 14:34:25 +0530

    Update stock_span.java

--- stacks-and-queues/stock_span.java
+++ stacks-and-queues/stock_span.java
@@ -1,1 +1,6 @@
-/* 
+/* You are given a number n, representing the size of array a.
+You are given n numbers, representing the prices of a share on n days.
+You are required to find the stock span for n days.
+Stock span is defined as the number of days passed between the current day and the first day before today when the price was higher than today. */
+
+
