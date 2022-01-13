ce1f18b15306349fcfa200748be96b02d8627eca
Mahima Sahu <93143005+mahima672000@users.noreply.github.com>
2022-01-13 19:48:06 +0530

    Create add_last_in_linked_list.java

---
+++ Linked Lists/add_last_in_linked_list.java
@@ -0,0 +1,4 @@
+/* Insertion in linked lists */
+/* You are given a partially written LinkedList class.
+You are required to complete the body of addLast function. This function is supposed to add an element to the end of LinkedList.
+You are required to update head, tail and size as required. */
